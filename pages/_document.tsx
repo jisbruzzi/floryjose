589a75f5f01c05c02244c7ee7b476d176587b8d1
jisbruzzi <jose.sbru@gmail.com>
2021-09-26 23:49:59 -0300

    Version 1

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <body>
+        <body className="background-with-pattern">
           <Main />
           <NextScript />
         </body>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,132 +1,19 @@
 import type { GrayMatterFile } from "gray-matter"
 import React, { PropsWithChildren, useReducer } from "react"
-import Card from "../components/landing/Card"
-import ContentContainer from "../components/landing/ContentContainer"
-import Curriculum from "../components/landing/Curriculum"
-import Hero from "../components/landing/Hero"
-import PhotoAndInformation from "../components/landing/PhotoAndInformation"
-import PortfolioItem from "../components/landing/PortfolioItem"
-import ReadMore from "../components/landing/ReadMoreButton"
 import { getBySlug, getPortfolioItems, getTech } from "../lib/landingParts"
 import markdownToHtml from "../lib/markdownToHtml"
 
-function SectionTitle(props: PropsWithChildren<{}>) {
-  return <h2 className="
-  text-center text-4xl 
-  font-montserrat m-4 font-extrabold 
-  pb-8 print:pb-0
-  print:my-0
-  ">
-    {props.children}
-  </h2>
-}
-
-function DarkBlock({ text }: { text: GrayMatterFile<string> }) {
-  return <div
-    className="bg-blue-900 text-white md:p-8 p-4 print:py-0">
-    <ContentContainer>
-      {
-        text.data.title &&
-        <SectionTitle>
-          <span className="filter print:drop-shadow-none drop-shadow-strong">
-            {text.data.title}
-          </span>
-        </SectionTitle>
-      }
-      <div
-        className="prose prose-white prose-compact max-w-none print:prose-super-compact print:prose-bluedots"
-        dangerouslySetInnerHTML={{ __html: text.content }}
-      />
-    </ContentContainer>
-  </div>
-}
-
-interface Technology {
-  name: string,
-  knowledge: number,
-}
-
 interface HomeProps {
-  jose: {
-    main: GrayMatterFile<string>,
-    detail: GrayMatterFile<string>,
-  },
-  experience: GrayMatterFile<string>,
-  portfolio: GrayMatterFile<string>[],
-  technologies: Technology[]
-}
-
-function Portfolio(props: { portfolioItems: GrayMatterFile<string>[] }) {
-  return <div className="bg-blue-50 md:p-8 p-4 print:py-0">
-    <ContentContainer>
-      <SectionTitle> Portfolio </SectionTitle>
-      {props.portfolioItems.map(item => <PortfolioItem information={item} />)}
-    </ContentContainer>
-  </div>
-}
-
-function Tech(props: { technologies: Technology[] }) {
-  return <div className="bg-blue-50 md:p-8 p-4 print:py-0 avoid-break-inside">
-    <ContentContainer>
-      <SectionTitle> Technologies </SectionTitle>
-      <div className="flex flex-wrap justify-evenly">
-        {props.technologies.sort((a, b) => b.knowledge - a.knowledge).map(tech => <div 
-          className="
-          bg-white 
-          m-2 print:m-1
-          text-xl print:text-base
-          p-3 print:p-1
-          rounded-full 
-          shadow-xl print:shadow-none
-          print:border-2
-          print:border-gray-100
-          "
-        >
-          {tech.name}
-        </div>)}
-      </div>
-    </ContentContainer>
-  </div>
-}
-
-function LetsTalkFooter(props: {}) {
-  return <div
-    className="bg-blue-900 text-white pb-60 pt-20 print:hidden">
-    <ContentContainer>
-
-      <div className="
-        text-center text-2xl 
-        m-4
-      ">
-        I can turn your idea into <span className="font-bold">reality</span>.
-      </div>
-      
-      <div className="
-        text-center text-8xl 
-        font-montserrat m-4 font-extrabold 
-        filter drop-shadow-strong
-      ">
-        Let's Talk
-      </div>
-      <div
-        className="text-center prose prose-white prose-compact max-w-none text-xl"
-      >
-        Send me an email to <a href="mailto:jose.sbru@gmail.com">jose.sbru@gmail.com</a>
-      </div>
-
-    </ContentContainer>
-  </div>
+  main: GrayMatterFile<string>,
 }
 
 export default function Homepage(props: HomeProps) {
-  console.log(props)
-  return <>
-    <Curriculum main={props.jose.main} detail={props.jose.detail} />
-    <DarkBlock text={props.experience} />
-    <Portfolio portfolioItems={props.portfolio} />
-    <Tech technologies={props.technologies} />
-    <LetsTalkFooter />
-  </>
+  return <div className="mx-auto bg-white">
+    <div
+    className="prose  my-8 prose-compact notprint:prose-weaker print:my-2 print:prose-super-compact print:prose-bluedots"
+      dangerouslySetInnerHTML={{ __html: props.main.content }}
+    />
+  </div>
 }
 
 export async function getStaticProps(): Promise<{ props: HomeProps }> {
@@ -139,30 +26,10 @@ export async function getStaticProps(): Promise<{ props: HomeProps }> {
       orig: ""
     }
   }
-  function mapTechnologies(): Technology[] {
-    const tech = getTech();
-    if (tech instanceof Array) {
-      return tech.map((value) => {
-        if (value instanceof Array && typeof value[0] === "string" && typeof value[1] === "number") {
-          return {
-            name: value[0],
-            knowledge: value[1],
-          }
-        }
-      }).reduce<Technology[]>((acum, next) => next == undefined ? acum : [...acum, next], [])
-    }
-    return []
-  }
 
   return {
     props: {
-      jose: {
-        main: (await htmlPart("jose.md")),
-        detail: (await htmlPart("jose.details.md")),
-      },
-      experience: (await htmlPart("experience.md")),
-      portfolio: await Promise.all(getPortfolioItems().map(name => htmlPart(`portfolio/${name}`))),
-      technologies: mapTechnologies()
+      main:await htmlPart("main.md")
     },
   }
 }
