8e9e8bc883511e8df1a53eff11d115fc916080aa
jisbruzzi <jose.sbru@gmail.com>
2021-10-03 21:30:04 +0000

    Deploying to gh-pages from @ jisbruzzi/floryjose@769bbc9ade83fe63af15cbb92e517fade4b53365 🚀

---
+++ _next/static/LcYlUv3yBp_eG-1SaTAlP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-0140fb471429c2cf3192.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-91d3c1b802ffbf32b88d.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
